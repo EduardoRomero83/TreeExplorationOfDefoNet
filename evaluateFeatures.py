eb1e6d773f87c881b01aa394d98a07ef5f628a9e
EduardoRomero83 <earg8395@hotmail.com>
2023-03-12 17:03:09 -0400

    Add logs

--- evaluateFeatures.py
+++ evaluateFeatures.py
@@ -17,7 +17,9 @@ res = dns.resolver.Resolver(configure=False)
 res.nameservers = [ kubedns ]
 
 
-
+print ("Content-type: text/html")
+print ("")
+print ("preparing execution")
 
 form = cgi.FieldStorage()
 item = str(form.getvalue("ms"))
@@ -26,6 +28,7 @@ port = str(form.getvalue("port"))
 item = item + ".default.svc.cluster.local"
 r = res.query(item, 'A')
 ipaddr = str(r[0])
+print ("starting execution")
 with urllib.request.urlopen('http://'+ipaddr+':'+port+'/cgi-bin/executeTree.py') as response:
    html = response.read()
 
