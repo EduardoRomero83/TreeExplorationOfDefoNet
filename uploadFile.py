d13b34f5710e39672c7a49a14a9dbecada0d6cd9
EduardoRomero83 <earg8395@hotmail.com>
2023-03-19 15:45:15 -0400

    add unzipping

--- uploadFile.py
+++ uploadFile.py
@@ -5,26 +5,29 @@ Created on Sat Mar 18 17:05:13 2023
 
 @author: Eduardo Romero
 """
-
-import cgi, os
-import cgitb; cgitb.enable()
+#!/usr/bin/env python
+import cgi
+import os
 import zipfile
+import io
+import shutil
 
-UPLOAD_DIR = "/opt/bitnami/apache2/cgi-bin/"
+UPLOAD_DIR = "/path/to/upload/directory"
+UNZIP_DIR = "/path/to/unzip/directory"
 
 print("Content-Type: text/html")
 print()
 
 print("<html>")
 print("<head>")
-print("<title>Upload ZIP File</title>")
+print("<title>Upload and Unzip ZIP File</title>")
 print("</head>")
 print("<body>")
 
-print("<h1>Upload ZIP File</h1>")
+print("<h1>Upload and Unzip ZIP File</h1>")
 print("<form method='post' enctype='multipart/form-data'>")
 print("<input type='file' name='file'>")
-print("<input type='submit' value='Upload'>")
+print("<input type='submit' value='Upload and Unzip'>")
 print("</form>")
 
 form = cgi.FieldStorage()
@@ -44,7 +47,15 @@ else:
         # Save the file to the upload directory
         with open(os.path.join(UPLOAD_DIR, filename), "wb") as f:
             f.write(file.file.read())
-        print("<p>File uploaded successfully.</p>")
+        
+        # Reset the file position before extracting
+        file.file.seek(0)
+        
+        # Unzip the file into the specified directory
+        with zipfile.ZipFile(io.BytesIO(file.file.read())) as zip_ref:
+            zip_ref.extractall(UNZIP_DIR)
+        
+        print("<p>File uploaded and unzipped successfully.</p>")
 
 print("</body>")
 print("</html>")
